31c2436ce13d05c9e3fe250abcbcfe94455df540
SCORPION <jopaok228@gmail.com>
2025-08-16 10:30:33 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~B6lkXbYR#YExHVzOUXt1MT-GDETIMxvlVFMR0dPP4_CALRX1Nlrg",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
